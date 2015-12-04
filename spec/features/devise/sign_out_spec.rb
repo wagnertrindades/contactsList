d0491f5fcc9959dc9a2b5c06cc37956e4b3490e7
Wagner Trindade <wtrindades@hotmail.com>
2015-12-04 09:08:52 -0200

    Added test to sign out and associated code

---
+++ spec/features/devise/sign_out_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'sessions#destroy' do
+
+    context 'Log out the account' do
+        it 'success' do
+            sign_in
+
+            click_link "Sair"
+
+            expect(page).to have_content("Saiu com sucesso")
+        end
+    end
+end
