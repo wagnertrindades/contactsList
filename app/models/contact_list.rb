9c1477b3cd993953e140bdb238b3069b95bb13a6
Wagner Trindade <wtrindades@hotmail.com>
2015-12-04 08:37:23 -0200

    Added ContactList model

---
+++ app/models/contact_list.rb
@@ -0,0 +1,3 @@
+class ContactList < ActiveRecord::Base
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203203559) do
+ActiveRecord::Schema.define(version: 20151204103515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_lists", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "contact_lists", ["user_id"], name: "index_contact_lists_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 20151203203559) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "contact_lists", "users"
 end
